407d44ebd11d6a9a230d093bcf6644b7eafeef11
Kirill Kouzoubov <kirill888@gmail.com>
2020-02-27 14:58:55 +1100

    Take out bash code from yaml
    Rather than writing computed envs to temp file to be re-used by other steps,
    just recompute them on every step by sourcing ci-helpers.sh script.

---
+++ .github/workflows/ci-helpers.sh
@@ -0,0 +1,31 @@
+# This script is sourced by every step.
+#
+# It defines how git branch maps to docker tag and provides some bash functions
+
+BUILDER_TAG0=_build_cache
+branch="${GITHUB_REF/refs\/heads\//}"
+
+if [ branch = "master" ]; then
+    MAIN_TAG="latest"
+    BUILDER_TAG="${BUILDER_TAG0}"
+else
+    MAIN_TAG="${branch}"
+    BUILDER_TAG="${BUILDER_TAG0}_${branch}"
+fi
+
+SUDO_TAG=${MAIN_TAG}-sudo
+
+pull_docker_cache () {
+    local image=${1}
+    local fallback=${2}
+
+    if docker pull $image ; then
+        echo "Pulled ${image}"
+    else
+        echo "Pulling fallback ${fallback}"
+        if docker pull $fallback ; then
+            echo "Using fallback: ${fallback}"
+            docker tag $fallback $image
+        fi
+    fi
+}
