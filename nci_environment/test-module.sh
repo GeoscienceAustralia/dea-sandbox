649387e5749d3806d8dd7bcf1d1ae70c306c9f12
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2023-11-24 03:27:02 +0000

    update test script to not need full module path and test against notebooks

--- nci_environment/test-module.sh
+++ nci_environment/test-module.sh
@@ -2,6 +2,10 @@
 
 
 # Module to test
+if [[-z $1 ]]; then
+    echo "ERROR: no module provided"
+    exit 1
+fi
 module_to_test="$1"
 
 echo Testing module: "$module_to_test"
@@ -10,7 +14,7 @@ echo Testing module: "$module_to_test"
 export MODULEPATH=/apps/.mf:/opt/Modules/modulefiles:/apps/Modules/modulefiles:
 
 # Ensure that all files are world readable
-if [[ $(find "$module_to_test" ! -perm /004) ]]; then
+if [[ $(find /g/data/v10/public/modules/modulefiles/"$module_to_test" ! -perm /004) ]]; then
     echo "ERROR: Some files in $module_to_test are not world readable."
     exit 1
 fi
@@ -26,6 +30,13 @@ module load "$module_to_test"
 # TODO Check the output from:
 datacube system check
 
+echo Testing notebooks
+cd ../..
+export DEA_TOOLS_TIDE_MODELS=/g/data/v10/tide_models
+if [[ ! -d dea-notebooks ]]; then
+    git clone --depth 1 https://github.com/GeoscienceAustralia/dea-notebooks.git
+fi
+./dea-notebooks/Tests/test_notebooks.sh
 
 # Run the initialise Test Database Script
 
