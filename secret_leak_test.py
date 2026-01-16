46db3f86b0115b96658c740572a44216e13f5f38
Xavier Gonzalez <xavier.gonzalez@ga.gov.au>
2026-01-16 01:57:39 +0000

    added test bad code

---
+++ secret_leak_test.py
@@ -0,0 +1,11 @@
+def leak_secrets():
+    fake_discord_client_secret = '8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ'   
+    fake_aws_secret = 'AKIAI234567890123456'
+    return fake_aws_secret, fake_discord_client_secret
+
+def main()
+    print(leak_secrets)
+    return
+
+if __name__ == '__main__':
+    main()
