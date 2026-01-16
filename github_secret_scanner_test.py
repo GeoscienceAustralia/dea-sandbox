0ddf4d3f360b2085d095d80a00e5070ef261d478
Xavier Gonzalez <xavier.gonzalez@ga.gov.au>
2026-01-16 02:50:05 +0000

    testing github builting scanning

--- github_secret_scanner_test.py
+++ github_secret_scanner_test.py
@@ -1,6 +1,7 @@
 def leak_secrets():
     test_discord_client_secret = '8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ'   
-    test_aws_secret = 'AKIAI234567890123456'
+    test_aws_token = 'ASIA1J8DE8JRS2LD02CX'
+    test_aws_secret = '75hTuwJ=13UYCEIapw+i=whe00lnj=cDfTEu9vRF'
     return fake_aws_secret, fake_discord_client_secret
 
 def main()
